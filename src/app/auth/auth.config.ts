1b280dab56983605a873ed74210541c0fcf22dc0
Darkflam3master <x.charlie42@gmail.com>
2025-11-20 09:57:27 -0500

    update auth config

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -1,15 +1,16 @@
 import { PassedInitialConfig } from 'angular-auth-oidc-client';
+import { environment } from '../../environments/environment';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-              authority: 'https://localhost',
-              redirectUrl: window.location.origin,
-              postLogoutRedirectUri: window.location.origin,
-              clientId: 'please-enter-clientId',
-              scope: 'please-enter-scopes', // 'openid profile offline_access ' + your scopes
-              responseType: 'code',
-              silentRenew: true,
-              useRefreshToken: true,
-              renewTimeBeforeTokenExpiresInSeconds: 30,
-          }
-}
+    authority: 'http://localhost:8080/realms/shadow-realm',
+    redirectUrl: environment.appUrl,
+    postLogoutRedirectUri: environment.appUrl,
+    clientId: 'charles-ag',
+    scope: 'oppenid profile email', // 'openid profile offline_access ' + your scopes
+    responseType: 'code',
+    silentRenew: true,
+    useRefreshToken: true,
+    renewTimeBeforeTokenExpiresInSeconds: 30,
+  },
+};
